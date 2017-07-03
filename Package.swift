7e2ca843d0cda06bf24cf933d49f0f7efbfa1932
Christopher Prince <chris@SpasticMuffin.biz>
2017-07-03 15:56:09 -0600

    Added dependencies in Swift.package. Renamed module folder for Swift package.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
 import PackageDescription
 
+// When used with the server, this needs to be built with the flag -DSERVER
+
 let package = Package(
     name: "SyncServer_Shared",
     dependencies: [
     	.Package(url: "https://github.com/crspybits/Gloss.git", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 2, minor: 0)
     ]
 )
