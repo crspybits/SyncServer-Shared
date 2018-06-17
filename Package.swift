9ee3e651315683efe9b807c85bcae2d24b7091d3
Christopher Prince <chris@SpasticMuffin.biz>
2018-06-17 17:05:19 -0600

    Revert from Perfect change.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // .Package(url: "https://github.com/hkellaway/Gloss.git", majorVersion: 1, minor: 2),
     	.Package(url: "https://github.com/crspybits/Gloss.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 4),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 3, minor: 1)
+        .Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 2, minor: 0)
         
     ]
 )
