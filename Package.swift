4372e73fc4c6a7019ec5fee041c31c9550907c94
Christopher Prince <chris@SpasticMuffin.biz>
2018-06-18 06:48:35 -0600

    version 7.0.0

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
         // .Package(url: "https://github.com/hkellaway/Gloss.git", majorVersion: 1, minor: 2),
     	.Package(url: "https://github.com/crspybits/Gloss.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 2, minor: 4),
-        
-        // Not yet updating to the current version of Perfect, because it looks like it isn't compatible with the current version of Kitura. See https://github.com/PerfectlySoft/Perfect-MySQL/issues/27
         .Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 3, minor: 1)
-        // .Package(url: "https://github.com/PerfectlySoft/Perfect.git", majorVersion: 2, minor: 0)
     ]
 )
