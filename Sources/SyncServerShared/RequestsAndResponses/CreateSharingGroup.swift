bb457e18ba5f1a08acae37652dab893470bbfc3d
Christopher Prince <chris@SpasticMuffin.biz>
2018-08-18 15:09:27 -0600

    Changed create sharing group request to not require a name.

--- Sources/SyncServerShared/RequestsAndResponses/CreateSharingGroup.swift
+++ Sources/SyncServerShared/RequestsAndResponses/CreateSharingGroup.swift
@@ -12,7 +12,7 @@ import Gloss
 import Kitura
 #endif
 
-// The current signed in user is requesting creation of a sharing group.
+// The current signed in user is requesting creation of a sharing group. A sharing group name is optional because some clients may not need or want a name per sharing group.
 
 public class CreateSharingGroupRequest : NSObject, RequestMessage {
     // I'm having problems uploading complex objects in url parameters. So not sending a SharingGroup object yet. If I need to do this, looks like I'll have to use the request body and am not doing that yet.
@@ -41,11 +41,11 @@ public class CreateSharingGroupRequest : NSObject, RequestMessage {
     }
     
     public func nonNilKeys() -> [String] {
-        return [CreateSharingGroupRequest.sharingGroupNameKey]
+        return []
     }
     
     public func allKeys() -> [String] {
-        return self.nonNilKeys()
+        return self.nonNilKeys() + [CreateSharingGroupRequest.sharingGroupNameKey]
     }
 }
 
