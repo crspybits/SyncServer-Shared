3042e256e012a5af4ed5a94dda23384bc3c4205d
Christopher Prince <chris@SpasticMuffin.biz>
2018-09-14 20:16:47 -0600

    Edits to Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,12 @@ let package = Package(
         .package(url: "https://github.com/PerfectlySoft/Perfect.git", from: "3.1.0")
     ],
     targets: [
-        .target(name: "SyncServer_Shared")
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SyncServer_Shared",
+            dependencies: ["PerfectLib", "Kitura", "Gloss"],
+            path: "Sources/SyncServerShared"
+        ),
     ]
 )
