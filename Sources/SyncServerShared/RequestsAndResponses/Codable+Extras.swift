feca3d741bd5e645f5d7ba5a4eb220d60b3a936d
Christopher Prince <chris@SpasticMuffin.biz>
2019-02-24 10:14:38 -0700

    Added public qualifiers.

--- Sources/SyncServerShared/RequestsAndResponses/Codable+Extras.swift
+++ Sources/SyncServerShared/RequestsAndResponses/Codable+Extras.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-extension Encodable {
-    var toDictionary: [String: Any]? {
+public extension Encodable {
+    public var toDictionary: [String: Any]? {
         // TODO: Need to set up date encoding.
         guard let data = try? JSONEncoder().encode(self) else { return nil }
         return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
     }
 }
 
-class DictionaryDecoder {
+public class DictionaryDecoder {
     // TODO: Need to set up date encoding.
     private let jsonDecoder = JSONDecoder()
 
     /// Decodes given Decodable type from given array or dictionary
-    func decode<T>(_ type: T.Type, from json: Any) throws -> T where T: Decodable {
+    public func decode<T>(_ type: T.Type, from json: Any) throws -> T where T: Decodable {
         let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
         return try jsonDecoder.decode(type, from: jsonData)
     }
