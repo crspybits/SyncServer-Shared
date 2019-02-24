7dcf1215376e122e0424d74e800e9ce46e3822ea
Christopher Prince <chris@SpasticMuffin.biz>
2019-02-24 10:22:53 -0700

    Added init method and date formatting.

--- Sources/SyncServerShared/RequestsAndResponses/Codable+Extras.swift
+++ Sources/SyncServerShared/RequestsAndResponses/Codable+Extras.swift
@@ -9,15 +9,24 @@ import Foundation
 
 public extension Encodable {
     public var toDictionary: [String: Any]? {
-        // TODO: Need to set up date encoding.
+        let encoder = JSONEncoder()
+        let formatter = DateExtras.getDateFormatter(format: .DATETIME)
+        encoder.dateEncodingStrategy = .formatted(formatter)
+        
         guard let data = try? JSONEncoder().encode(self) else { return nil }
         return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
     }
 }
 
 public class DictionaryDecoder {
-    // TODO: Need to set up date encoding.
-    private let jsonDecoder = JSONDecoder()
+    public init() {}
+    
+    private let jsonDecoder:JSONDecoder = {
+        let decoder = JSONDecoder()
+        let formatter = DateExtras.getDateFormatter(format: .DATETIME)
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        return decoder
+    }()
 
     /// Decodes given Decodable type from given array or dictionary
     public func decode<T>(_ type: T.Type, from json: Any) throws -> T where T: Decodable {
