88a4b0ee618f90d6cc5efebda628fe9b5426616d
Christopher Prince <chris@SpasticMuffin.biz>
2019-03-08 18:02:48 -0700

    Updating UploadDeletion

--- Sources/SyncServerShared/RequestsAndResponses/UploadDeletion.swift
+++ Sources/SyncServerShared/RequestsAndResponses/UploadDeletion.swift
@@ -22,15 +22,18 @@ public class UploadDeletionRequest : RequestMessage, Filenaming {
     
     // This must indicate the current version of the file in the FileIndex.
     public var fileVersion:FileVersionInt!
+    private static let fileVersionKey = "fileVersion"
     
     // Overall version for files for the sharing group; assigned by the server.
     public var masterVersion:MasterVersionInt!
-    
+    private static let masterVersionKey = "masterVersion"
+
     public var sharingGroupUUID: String!
 
 #if DEBUG
     // Enable the client to actually delete files-- for testing purposes. The UploadDeletionRequest will not queue the request, but instead deletes from both the FileIndex and from cloud storage.
     public var actualDeletion:Bool?
+    private static let actualDeletionKey = "actualDeletion"
 #endif
 
     public func valid() -> Bool {
@@ -40,9 +43,21 @@ public class UploadDeletionRequest : RequestMessage, Filenaming {
         
         return true
     }
+    
+    private static func customConversions(dictionary: [String: Any]) -> [String: Any] {
+        var result = dictionary
+        
+        // Unfortunate customization due to https://bugs.swift.org/browse/SR-5249
+        MessageDecoder.convert(key: fileVersionKey, dictionary: &result) {FileVersionInt($0)}
+        MessageDecoder.convert(key: masterVersionKey, dictionary: &result) {MasterVersionInt($0)}
+#if DEBUG
+        MessageDecoder.convert(key: actualDeletionKey, dictionary: &result) {Bool($0)}
+#endif
+        return result
+    }
 
     public static func decode(_ dictionary: [String: Any]) throws -> RequestMessage {
-        return try MessageDecoder.decode(UploadDeletionRequest.self, from: dictionary)
+        return try MessageDecoder.decode(UploadDeletionRequest.self, from: customConversions(dictionary: dictionary))
     }
 }
 
@@ -55,8 +70,18 @@ public class UploadDeletionResponse : ResponseMessage {
     
     // If the master version for the user on the server has been incremented, this key will be present in the response-- with the new value of the master version. The upload deletion was not attempted in this case.
     public var masterVersionUpdate:Int64?
-    
+    private static let masterVersionUpdateKey = "masterVersionUpdate"
+
+    private static func customConversions(dictionary: [String: Any]) -> [String: Any] {
+        var result = dictionary
+        
+        // Unfortunate customization due to https://bugs.swift.org/browse/SR-5249
+        MessageDecoder.convert(key: masterVersionUpdateKey, dictionary: &result) {MasterVersionInt($0)}
+
+        return result
+    }
+
     public static func decode(_ dictionary: [String: Any]) throws -> UploadDeletionResponse {
-        return try MessageDecoder.decode(UploadDeletionResponse.self, from: dictionary)
+        return try MessageDecoder.decode(UploadDeletionResponse.self, from: customConversions(dictionary: dictionary))
     }
 }
